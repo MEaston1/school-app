505c7de080c4086b0692c4010d4f0b19b52c6484
MEaston1 <mattheweaston21@gmail.com>
2019-12-27 18:13:26 +0000

    christmas updates

--- Dissertation Project/app/src/main/java/com/example/schoolapp/MoreActivity.kt
+++ Dissertation Project/app/src/main/java/com/example/schoolapp/MoreActivity.kt
@@ -1,8 +1,14 @@
 package com.example.schoolapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MoreActivity : AppCompatActivity() {
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdaptter: RecyclerView.Adapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more)
+    }
 
 }
