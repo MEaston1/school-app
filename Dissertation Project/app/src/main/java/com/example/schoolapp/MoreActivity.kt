283c54998537378a79955422a6ef4d8ebf5d6536
MEaston1 <mattheweaston21@gmail.com>
2020-02-22 15:31:44 +0000

    button fix

--- Dissertation Project/app/src/main/java/com/example/schoolapp/MoreActivity.kt
+++ Dissertation Project/app/src/main/java/com/example/schoolapp/MoreActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MoreActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdaptter: RecyclerView.Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_more)
